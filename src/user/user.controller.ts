9d6249bd05eb76f8ba9a69e888dbbaef773dae17
DavidIsrael <davidisrael.rj@gmail.com>
2024-11-14 11:20:44 -0300

    Rota de leitura User

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 @Controller('users')
 export class UserController {
     @Post()
     async create(@Body() body){
         return {body};
+    }
+
+    @Get()
+    async list(){
+        return {users:[{"id":"1","name":"David",}]}
+    }
+
+    @Get(':id')
+    async show(@Param() param ){
+        return {user:{}, param}
     }
 }
