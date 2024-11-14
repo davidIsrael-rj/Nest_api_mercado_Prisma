19cabbf403be1cf8cbc547d7183d7baaa14f8d72
DavidIsrael <davidisrael.rj@gmail.com>
2024-11-14 13:52:55 -0300

    Rota list Product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,39 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 @Controller('product')
 export class ProductController {
 
     @Post()
-    async create(@Body() body){
-        return {body}
+    async create(@Body() body) {
+        return { body }
+    }
+
+    @Get()
+    async list() {
+        return {
+            product: [
+                {
+                    "id": "1",
+                    "name": "Caderneta",
+                    "precoCusto": "1.50",
+                    "precoVenda": "2.50",
+                    "setor": "papelaria",
+                    "fornecedor": "Papelaria China"
+                },
+                {
+                    "id": "2",
+                    "name": "Caderno",
+                    "precoCusto": "2.50",
+                    "precoVenda": "3.50",
+                    "setor": "papelaria",
+                    "fornecedor": "Papelaria China"
+                },
+            ]
+        }
+    }
+
+    @Get(':id')
+    async show(@Param() param){
+        return {product: {}, param}
     }
 }
