197d0dd059c6a64f7c3bf1a63807a09826eb3915
DavidIsrael <davidisrael.rj@gmail.com>
2024-11-15 06:50:46 -0300

    Rota list Sector

--- src/sector/sector.controller.ts
+++ src/sector/sector.controller.ts
@@ -1,9 +1,19 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post } from "@nestjs/common";
 
 @Controller('sector')
 export class SectorController {
     @Post()
     async create(@Body() body) {
         return { body }
+    }
+
+    @Get()
+    async list() {
+        return { sector: [{ "id": "1", "name": "papelaria" }] }
+    }
+
+    @Get(':id')
+    async show(@Param() params) {
+        return { sector: {}, params }
     }
 }
