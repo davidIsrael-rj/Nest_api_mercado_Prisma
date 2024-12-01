16ff6a5ac34f2d5c2e0ed15bb8aafe1905e3ac3a
DavidIsrael <davidisrael.rj@gmail.com>
2024-12-01 00:30:52 -0300

    Criando DTO’s do auth e o controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Post } from "@nestjs/common";
+import { AuthLoginDTO } from "./dto/auth-login.dto";
+import { AuthRegisterDTO } from "./dto/auth-register.dto";
+import { AuthForgetDTO } from "./dto/auth-forget.dto";
+import { AuthResetDTO } from "./dto/auth-reset.dto";
+
+@Controller('auth')
+export class AuthController {
+
+
+    @Post('login')
+    async login(@Body() body: AuthLoginDTO) { }
+
+    @Post('register')
+    async register(@Body() body: AuthRegisterDTO) { }
+
+    @Post('forget')
+    async forget(@Body() body: AuthForgetDTO) { }
+
+    @Post('reset')
+    async reset(@Body() body: AuthResetDTO) { }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { Module } from "@nestjs/common";
 import { JwtModule } from "@nestjs/jwt";
+import { AuthController } from "./auth.controller";
 
 @Module({
     imports:[JwtModule.register({
         secret:"zNaoLA6SbG&/FpNTcQVuic{Lvt$/pji["
-    })]
+    })],
+    controllers: [AuthController]
 })
 export class AuthModule {
 
