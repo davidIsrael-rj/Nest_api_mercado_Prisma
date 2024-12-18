f38eb681325968723fed5e3f7c158b80e590a295
DavidIsrael <davidisrael.rj@gmail.com>
2024-12-17 21:36:28 -0300

    Habilitando o CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { LogInterceptor } from './interceptors/log.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor());
   await app.listen(3000);
 }
