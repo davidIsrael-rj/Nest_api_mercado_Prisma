e17b1ed00155231cdcd74d4ac8965f50595e6908
DavidIsrael <davidisrael.rj@gmail.com>
2024-12-23 20:03:01 -0300

    Gravando arquivo de imagem em uma pasta

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,6 +9,8 @@ import { AuthMeDTO } from "./dto/auth-me.dto";
 import { AuthGuard } from "../guards/auth.guard";
 import { User } from "../decorators/user.decorator";
 import { FileInterceptor } from "@nestjs/platform-express";
+import { writeFile } from "fs/promises";
+import { join } from 'path';
 
 @Controller('auth')
 export class AuthController {
@@ -16,39 +18,43 @@ export class AuthController {
     constructor(
         private readonly userService: UserService,
         private readonly authService: AuthService
-    ){}
+    ) { }
 
     @Post('login')
-    async login(@Body() {email, password}: AuthLoginDTO) {
+    async login(@Body() { email, password }: AuthLoginDTO) {
         return this.authService.login(email, password)
 
-     }
+    }
 
     @Post('register')
-    async register(@Body() body: AuthRegisterDTO) { 
+    async register(@Body() body: AuthRegisterDTO) {
         return this.authService.register(body);
     }
 
     @Post('forget')
-    async forget(@Body() {email}: AuthForgetDTO) {
+    async forget(@Body() { email }: AuthForgetDTO) {
         return this.authService.forget(email);
-     }
+    }
 
     @Post('reset')
-    async reset(@Body() {password, token}: AuthResetDTO) { 
+    async reset(@Body() { password, token }: AuthResetDTO) {
         return this.authService.reset(password, token);
     }
 
     @UseGuards(AuthGuard)
     @Post('me')
-    async me(@User() user){
-        return {user};
+    async me(@User() user) {
+        return { user };
     }
 
     @UseInterceptors(FileInterceptor('file'))
     @UseGuards(AuthGuard)
     @Post('photo')
-    async uploadPhoto(@User() user, @UploadedFile() photo: Express.Multer.File){
-        return {user, photo};
+    async uploadPhoto(@User() user, @UploadedFile() photo: Express.Multer.File) {
+
+
+        const result = await writeFile(join(__dirname, '..', '..', 'storage', 'photos', `photo-${user.id}.png`), photo.buffer);
+
+        return { user, photo, result };
     }
 }
