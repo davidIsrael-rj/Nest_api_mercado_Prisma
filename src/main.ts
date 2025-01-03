a4325faf922b381c35c9e8ee2a64309bb40ea49a
DavidIsrael <davidisrael.rj@gmail.com>
2025-01-03 20:52:45 -0300

    Ajustando API para porta 3001

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new LogInterceptor());
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
